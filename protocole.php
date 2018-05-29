9146e4e24e7972df59216b435989f6b9fcbd932d
Marie0401 <36883931+Marie0401@users.noreply.github.com>
2018-05-29 15:01:23 +0200

    le gras
    MO

--- protocole.php
+++ protocole.php
@@ -13,9 +13,9 @@
 			<hr class="divider">
 			<img src="images/screenshot.png" alt="screenshot" class="img-center img-fluid"><br><br>
 			<div class="text-center">Deux joueurs avec un score commun<br> <br>
-			Leur but : <br>Tuer un maximum d'ennemis, et en laisser passer le moins possible<br><br>
+			Leur but : <br><strong>Tuer</strong> un maximum d'ennemis, et en laisser passer le moins possible<br><br>
 			Deux modalités : <br>
-			Modalité A avec affichage de la zone d'attention, modalité B sans<br>
+			Modalité A <strong>avec affichage</strong> de la zone d'attention, et modalité B <strong>sans affichage.</strong><br>
 			Chaque équipe passe les deux modalités, la moitié dans l'ordre AB et l'autre dans l'ordre BA<br><br>
 			
 			But de l'expérience :<br>
@@ -28,11 +28,11 @@
 			<hr class="divider">
 			<h2 class="text-center text-lg text-uppercase my-0"> <strong>Questionnaire </strong></h2>
 			<hr class="divider">
-			<p>Dans ce projet, le but est de réussir à évaluer la collaboration des joueurs. Pour cela nous avons des données quantitatives, qui vont être les différents scores mis en place dans une partie de jeu. 
-			Mais nous avons également besoin de données qualitatives, récoltées auprès des joueurs.</br>
+			<p>Dans ce projet, le but est de réussir à <strong>évaluer la collaboration</strong> des joueurs. Pour cela nous avons des <strong>données quantitatives</strong>, qui vont être les différents scores mis en place dans une partie de jeu. 
+			Mais nous avons également besoin de <strong>données qualitatives</strong>, récoltées auprès des joueurs.</br>
 			Travaillant avec des être humains, il nous a semblé essentiel d'avoir un retour sur les ressentis et les impressions qu'ont pu vivre les personnes en situation de collaboration. Car la collaboration est aussi un sentiment de soutien et de travail d'équipe.
-			Nous avons donc mis en place une courte phase de restitution d'informations auprès des joueurs qui sera en deux phases.</br>
-			Tout d'abord ils devront répondre au questionnaie AttrakDiff pour évaluer notre jeu, puis à quelques questions ouvertes pour évaluer leur sentiment de collaboration.</p>
+			Nous avons donc mis en place une courte <strong>phase de restitution</strong> d'informations auprès des joueurs qui sera en deux phases.</br>
+			Tout d'abord ils devront répondre au questionnaie <strong>AttrakDiff</strong> pour évaluer notre jeu, puis à quelques <strong>questions ouvertes</strong> pour évaluer leur sentiment de collaboration.</p>
 			<div class="text-center"><a href="questionnaire.php"> Questionnaire de fin d'expérience </a></div>
 		</div>
 		
